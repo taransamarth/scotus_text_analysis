85a16c83f9c27e54d74beedb0d8fdf60ba006049
taran samarth <5815717+taransamarth@users.noreply.github.com>
2021-05-10 15:30:08 -0400

    Add files via upload

---
+++ cases.r
@@ -0,0 +1,144 @@
+require(tidyverse)
+require(ggplot2)
+require(haven)
+require(stringr)
+require(httr)
+require(jsonlite)
+require(quanteda)
+require(LSX)
+require(robustbase)
+require(stargazer)
+
+### Data cleaning and reduction
+
+# Get helpers
+setwd("C:/Users/Taran Samarth/Desktop/plsc476")
+source("helpers.R")
+
+# Pull databases and merge
+scdb <- rbind(read.csv("SCDB_justicecentered.csv"),read.csv("SCDB_legacy_justicecentered.csv"))
+
+jdb <- read_xpt("http://epstein.wustl.edu/research/justicesdata.v8xpt")
+
+merged <- merge(scdb,jdb,by.x="justice",by.y="spaethid")
+
+# Pull president term data and convert strings to Date objects
+## This allows us to check under which President a case was argued (done using a helper function in the other file)
+presidents <- read.csv("presidential.csv")
+presidents$start <-as.Date(presidents$start,"%m/%d/%Y")
+presidents$end <-as.Date(presidents$end,"%m/%d/%Y")
+
+# Merge most recent Segal-Cover scores (retrieved here: http://scdb.wustl.edu/data.php?s=5)
+segal <- read.csv("measures.csv") 
+merged <- merge(merged, subset(segal,select=-c(justiceName)), by=c("justice","term")) 
+
+# Merge most recent DW-NOMINATE scores for Presidents (retrieved from link below)
+nominate<-read.csv("https://voteview.com/static/data/out/members/HSall_members.csv")
+presidents <- cbind(presidents,nominate %>%  filter(chamber == "President") %>% slice(-(1:67)) %>% select(c(bioname,nominate_dim1)) %>% separate(bioname,c("bioname",NA),",") %>% mutate(bioname = tolower(bioname)) %>% group_by(bioname, nominate_dim1) %>% distinct())
+
+# Import salience measures
+salience <- read.csv("scdbSalience.csv")
+merged <- merged %>% inner_join(salience, by="caseId")
+
+# Create subset of all cases where federal government was a party, create variables, ensure that only one row is preserved for each vote
+## fed_pet: 1 = federal gov. is petitioner, 0 = respondent
+## fed_win: 1 = Court rules for federal gov., 0 = Court rules against federal gov.
+
+relevant_parties <- c(1,27,304,316,324,325,326,327,328,329,330,362,363,364,371,408,422)
+
+fed_cases <- merged %>% filter((respondent %in% relevant_parties | petitioner %in% relevant_parties) & !(dateArgument=="")) %>%
+  mutate(dateArgument = as.Date(dateArgument,"%m/%d/%Y"),
+         fed_pet = as.numeric(petitioner %in% relevant_parties), 
+         fed_win = ifelse((fed_pet==1 & partyWinning == 1) | (fed_pet==0 & partyWinning==0), 1, 
+                          ifelse((fed_pet==1 & partyWinning == 0) | (fed_pet==0 & partyWinning == 1), 0, NA))) %>% distinct(voteId,.keep_all=T) %>% filter(dateArgument > as.Date("1920", "%Y"))
+
+# Tune ideological measures and calculate absolute value
+## exec_nom is the DW-NOMINATE score for the President in office when the case was heard
+## The Segal-Cover score is 0 for conservatives and 1 for liberals, while NOMINATE's ideological poles are reversed.
+## Thus, I flip the Segal-Cover score so both measures are oriented the same way and then calculate the absolute value between them.
+fed_cases <- fed_cases %>% mutate(exec_nom = fed_checker(dateArgument, "nominate_dim1"), scIdeology = (-1*scIdeology), diff = abs(scIdeology - exec_nom))
+
+# Add variables and remove cases where justices did not author/co-author decision
+## fed_par: 1 = Democratic executive, 0 = Republican executive
+## republican/democrat = 1 = Justice nominated by president of that party, 0 = not
+## fed_vote: 1 = Justice sided with federal gov., 0 = Justice sided against federal gov.
+fed_cases <- fed_cases %>% mutate(fed_par=fed_checker(dateArgument, "party"),
+                                  era = fed_checker(dateArgument, "sec"),
+                                  fed_dem=ifelse(fed_par=="Democratic",1,0), 
+                                  republican=ifelse(parnom==6,1,0),democrat=ifelse(parnom==1,1,0), 
+                                  fed_vote=ifelse((fed_win == 1 & majority == 2) | (fed_win == 0 & majority == 1),1,0)) %>% filter(opinion %in% c(2,3))
+
+# Create executive experience covariate
+fed_cases <- fed_cases %>% mutate(exec_exp = ifelse(usasat1 > 999 | usat1 > 999 | ussgo1 > 999 | ussg1 > 999 | usago1 > 999 | usag1 > 999 | uscab1 > 999 | ussec1 > 999 | usage1 > 999 | usagh1 > 999 | uspres1 > 999, 1, 0))
+
+# Subset cases into those where Justice-Executive were of opposing parties + of same party, create respective variables
+opp_cases <- fed_cases %>% filter((democrat==1 & fed_dem==0)|(republican==1 & fed_dem == 1)) %>% mutate(opp_case = 1, same_case = 0)
+same_cases <- fed_cases %>% filter((democrat==1 & fed_dem==1)|(republican==1 & fed_dem == 0)) %>% mutate(opp_case = 0, same_case = 1)
+
+### Modeling
+
+# Create dictionary
+
+# Original dictionary used to identify words, later dictionary was developed after picking out n-grams or clearly incorrect terms that got picked up (e.g. "great_britain")
+## dict <- as.dictionary(dict <- data.frame(word=c("bad*", "awful*", "negativ*", "horribl*", "wrong*", "terribl*", "poor*",  "incorrect", "err*", "fail*", "brilliant*", "great*", "terrific*", "wonderful*", "splendid*", "good*", "fantastic*", "excellent*", "correct*",  "positiv*"), sentiment=c("negative","negative","negative","negative","negative","negative","negative","negative","negative","negative","negative","positive","positive","positive","positive","positive","positive","positive","positive","positive","positive","positive")))
+dict <- data.frame(word = c("accurate", "accurately", "brilliant", "brilliantly", "wonderful", "wonderfully", "excellent", "excellently", "good", "great", "greatly", "positive", "positively", "correct", "correctly", "terrific", "fortunate", "fortunately", "superior", "nice", "nicely"), sentiment="positive")
+dict <- rbind(dict, data.frame(word=c("erred", "err", "negatively", "fail", "poorly", "negative", "terrible", "failure", "bad", "badly", "poor", "failing", "errors", "negatives", "error", "incorrect", "wrong", "erroneous", "wrongly", "errs", "failures", "fails", "failed", "erroneously"), sentiment = "negative"))
+dict <- as.dictionary(dict)
+
+# Combine the two previously subset dataframes back into one for LSS modeling
+## LSS rescales fits on the corpus to have mean 0 and std. dev. 1, so we can't just predict each subset separately.
+total_cases <- rbind(opp_cases, same_cases)
+
+# Fit model for k-cohesion plot with very high k
+lss_k <- fit_model(total_cases, as.seedwords(dict, upper=-1, lower=1), "the_government", 600)
+
+# Fit model using helper function
+lss_gov_comb <- fit_model(total_cases, as.seedwords(dict, upper=-1, lower=1), "the_government", 430)
+
+# Predict model using helper function
+total_pred <- predict_model(total_cases, lss_gov_comb)
+
+# Create updated dataframe with predictions, smoothed set, and split by exec-justice partisanship for smoothing
+fitted_df <- total_pred[[1]]
+
+fitted_smoothed <- total_pred[[2]]
+
+fitted_opp <- smooth_lss(total_pred[[1]] %>% filter(opp_case == 1), lss_var="fit", engine="loess",date="dateArgument")
+
+fitted_same <- smooth_lss(total_pred[[1]] %>% filter(opp_case == 0), lss_var="fit", engine="loess",date="dateArgument")
+
+### Graphics
+
+# Sentiment time-series for all texts
+fitted_smoothed %>% ggplot(aes(date,fit)) + geom_ribbon(aes(ymin=fit-1.96*se.fit, ymax=fit+1.96*se.fit), fill="grey80") + geom_line(color="black") +  geom_hline(yintercept=0,linetype="dotted") + geom_rect(data=presidents,aes(xmin=start,xmax=end,ymin=-Inf,ymax=Inf,fill=party),alpha=.2,inherit.aes=F) + scale_fill_manual(values=alpha(c("blue","red"),.1),name = "Executive Party") + coord_cartesian(ylim=c(-1,1), xlim=c(as.Date("1946", "%Y"), as.Date("2009","%Y")))
+
+# Sentiment time-series for opposing executive-justice texts
+fitted_opp %>% ggplot(aes(date,fit)) + geom_ribbon(aes(ymin=fit-1.96*se.fit, ymax=fit+1.96*se.fit), fill="grey80") + geom_line(color="black") +  geom_hline(yintercept=0,linetype="dotted") + geom_rect(data=presidents,aes(xmin=start,xmax=end,ymin=-Inf,ymax=Inf,fill=party),alpha=.2,inherit.aes=F) + scale_fill_manual(values=alpha(c("blue","red"),.1),name = "Executive Party") + coord_cartesian(ylim=c(-1,1), xlim=c(as.Date("1946", "%Y"), as.Date("2009","%Y")))
+
+# Sentiment time-series for same executive-justice texts
+fitted_same %>% ggplot(aes(date,fit)) + geom_ribbon(aes(ymin=fit-1.96*se.fit, ymax=fit+1.96*se.fit), fill="grey80") + geom_line(color="black") +  geom_hline(yintercept=0,linetype="dotted") + geom_rect(data=presidents,aes(xmin=start,xmax=end,ymin=-Inf,ymax=Inf,fill=party),alpha=.2,inherit.aes=F) + scale_fill_manual(values=alpha(c("blue","red"),.1),name = "Executive Party") + coord_cartesian(ylim=c(-1,1), xlim=c(as.Date("1946", "%Y"), as.Date("2009","%Y")))
+
+# Justice mean sentiment in opposing cases
+## Using what I remember from statistics, I take the document standard errors, square them, sum them by justice, divide by the number of documents squared, and then take the square root of the resultant variance to get standard errors for calculating confidence intervals.
+## I use the same strategy for the confidence intervals of all grouped means.
+fitted_df %>% filter(!is.na(var.fit), opp_case == 1) %>% group_by(name) %>% dplyr::summarize(mean = mean(fit, na.rm=TRUE),var.sum=sum(var.fit),n=n()) %>% filter(n>15) %>% ggplot(aes(reorder(name,-mean,sum),mean,ymin=mean-(1.96*(sqrt(var.sum/(n^2)))),ymax=mean+1.96*(sqrt(var.sum/(n^2)))))+ geom_pointrange() +geom_hline(yintercept=0, lty=2) + xlab("Justice") + ylab("Sentiment") + coord_flip()
+
+# Justice mean fit in same cases
+fitted_df %>% filter(!is.na(var.fit), opp_case == 0) %>% group_by(name) %>% dplyr::summarize(mean = mean(fit, na.rm=TRUE),var.sum=sum(var.fit),n=n()) %>% filter(n>15) %>% ggplot(aes(reorder(name,-mean,sum),mean,ymin=mean-(1.96*(sqrt(var.sum/(n^2)))),ymax=mean+1.96*(sqrt(var.sum/(n^2)))))+ geom_pointrange() +geom_hline(yintercept=0, lty=2) + xlab("Justice") + ylab("Sentiment") + coord_flip()
+
+# Justice mean fit in all cases
+fitted_df %>% filter(!is.na(var.fit)) %>% group_by(name) %>% dplyr::summarize(mean = mean(fit, na.rm=TRUE),var.sum=sum(var.fit),n=n()) %>% filter(n>50) %>% ggplot(aes(reorder(name,-mean,sum),mean,ymin=mean-(1.96*(sqrt(var.sum/(n^2)))),ymax=mean+1.96*(sqrt(var.sum/(n^2)))))+ geom_pointrange() +geom_hline(yintercept=0, lty=2) + xlab("Justice") + ylab("Sentiment") + coord_flip()
+
+# Executive party "eras"
+## This is just the time periods of partisan continuity in the executive
+fitted_df %>% filter(!is.na(var.fit)) %>% group_by(era) %>% dplyr::summarize(mean = mean(fit, na.rm=TRUE),var.sum=sum(var.fit),n=n()) %>% filter(n>15) %>% ggplot(aes(reorder(era,-mean,sum),mean,ymin=mean-(1.96*(sqrt(var.sum/(n^2)))),ymax=mean+1.96*(sqrt(var.sum/(n^2)))))+ geom_pointrange() +geom_hline(yintercept=0, lty=2)  + coord_flip()
+
+# Means in opposing cases vs. same cases
+fitted_df %>% filter(!is.na(var.fit)) %>% group_by(opp_case) %>% dplyr::summarize(mean = mean(fit, na.rm=TRUE),var.sum=sum(var.fit),n=n()) %>% ggplot(aes(reorder(opp_case,-mean,sum),mean,ymin=mean-(1.96*(sqrt(var.sum/(n^2)))),ymax=mean+1.96*(sqrt(var.sum/(n^2)))))+ geom_pointrange() +geom_hline(yintercept=0, lty=2) + ylab("Sentiment") + xlab("Justice-Executive Partisanship") + scale_x_discrete(labels = c("Same Party", "Opposing Parties")) + coord_flip()
+
+### Regression
+# I use robust regressions (with the 'robustbase' package) as, despite LSS doing its best to fit sentiment onto a normal distribution, there are still enough outliers to justify robust regression here.
+## One regression uses ideological distance from DW-NOMINATE/Segal-Cover measures, the other just uses the binary indicator for same/opposing exec-justice partisanship
+robustreg_diff <- lmrob(fit~diff + fed_pet + nytSalience + exec_exp, family="gaussian", data = fitted_df)
+
+robustreg_opp <- lmrob(fit~opp_case + fed_pet + nytSalience + exec_exp, family="gaussian", data = fitted_df)
---
+++ helpers.R
@@ -0,0 +1,107 @@
+require(tidyverse)
+require(ggplot2)
+require(haven)
+require(stringr)
+require(httr)
+require(jsonlite)
+require(quanteda)
+require(LSX)
+require(robustbase)
+
+# Functions to identify which party holds executive branch when case argued before Court
+fed_date_check <- function(x, v) { 
+  for (row in 1:nrow(presidents)) {
+    if (between(x,presidents[row,"start"], presidents[row,"end"])) { 
+      return(presidents[row,v])
+    }
+  }
+}
+
+fed_checker <- function(x,v) { 
+  return(unlist(lapply(x, fed_date_check,v)))
+}
+
+# Get case text from case.law API for 1 case with cite + justice name
+get_case_text <- function(x,y) {
+  link <- paste("https://api.case.law/v1/cases/?cite=",x,"&full_case=true",sep="")
+  
+  justice <- paste("Justice", str_split(y,",")[[1]][1])
+  
+  text <- rawToChar(GET(link,add_headers(Authorization="Token 447b812d4bf02d3a4ed8f66f768c904ee0cd8f3e"))$content)
+  
+  Encoding(text) <- "UTF-8"
+  
+  opinion_df <- fromJSON(text)$results$casebody$data$opinions[[1]]
+  
+  if (is.null(opinion_df)) { return(NA)}
+  
+  if (length((opinion_df %>% filter(grepl(justice,author))))==0) { return(NA)}
+  
+  return((opinion_df %>% filter(grepl(justice,author)))$text)
+}
+
+# Helper to get case texts using vector of cites + justice names
+get_texts <- function(u,v) {
+  return(unlist(lapply(mapply(get_case_text,u,v,USE.NAMES = F), function (x) ifelse(length (x) > 0, x, NA))))
+}
+
+# Same preprocessing pipeline as Watanabe (2020) -- split into sentences, tokenize, identify and compound capitalized collocations, remove stopwords,
+# trim (the doc/term frequencies are generally arbitrary here)
+preprocess <- function(x) {
+  
+  # Retrieve texts
+  df <- x
+  df <- df %>% mutate(op_text = get_texts(usCite,name))
+  print(paste0("Missingness: ", (sum(is.na(df$op_text))/nrow(df))*100, "% of ", nrow(df), " cases."))
+  
+  # Filter out unretrievable observations
+  df <- df %>% filter(!is.na(op_text))
+  # saveRDS(df, file="sample.RDS")
+  
+  # Convert to sentence-level corpus
+  df_corp <- corpus(df,text_field = "op_text",docid_field = "voteId",unique_docnames = TRUE)
+  df_corp <- corpus_subset(df_corp, texts(df_corp) != "")
+  df_corp_reshape <- corpus_reshape(df_corp, to="sentences")
+  df_toks <- tokens(df_corp_reshape, remove_punct=T,remove_numbers=T)
+  
+  # Tokenize multiword pronouns
+  cap_toks <- df_toks %>% tokens_select(pattern="^([A-Z][a-z\\-]{2,})", valuetype = "regex", case_insensitive = FALSE, padding = TRUE) 
+  tstat <- textstat_collocations(cap_toks, tolower=FALSE, size=2:3, mincount=30) 
+  df_toks <- tokens_compound(df_toks, tstat, case_insensitive = T) %>% tokens_remove(stopwords("en"), padding = TRUE)
+  
+  # Remove single-letter terms + trim dfm to only reflect terms that appear with non-trivial frequencies
+  df_dfm <- tokens_select(df_toks, pattern = "\\b(\\w)\\b", selection = "remove", valuetype = "regex")
+  df_dfm <- dfm_trim(dfm(df_toks), min_termfreq = 10, min_docfreq = 5)
+  
+  return(list(df_toks, df_dfm, df))
+}
+
+# Model-fitting function
+## Preprocess data frame, identify tokens surrounding given term with statistical significance, and run model with given dictionary.
+fit_model <- function(x, dict, term, k) {
+  df <- x
+  p <- preprocess(df)
+  dfm <- p[[2]]
+  toks <- p[[1]]
+  term_context <- char_context(toks, pattern=term, p=0.05)
+  term_lss <- textmodel_lss(dfm, seeds=dict,terms=term_context,k=k, cache=T)
+  return(term_lss)
+}
+
+# Model prediction function
+## Given dataframe and model, fit model onto corpus of opinion texts.
+## Returns original dataframe with fits and LOESS smoothed time series.
+predict_model <- function(x, m) {
+  df <- x
+  p <- preprocess(df)
+  df_dfm <- p[[2]]
+  df <- p[[3]] %>% filter(!is.na(op_text))
+  df_dfm <- dfm_group(df_dfm)
+  predicted <- predict(m,newdata=df_dfm, se.fit = T)
+  df$fit <- predicted$fit
+  df$se <- predicted$se.fit
+  df <- df %>% filter(!is.na(fit), !is.na(se))
+  df$var.fit <- df$se^2
+  smoothed <- smooth_lss(df, lss_var="fit", engine="loess",date="dateArgument")
+  return(list(df, smoothed))
+}
